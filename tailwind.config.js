f23e848a305b5091295c8079f51f3b588898b31c
DoctorCo <78472859+DoctorCo@users.noreply.github.com>
2022-01-19 11:57:06 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      xd: "950px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [],
+};
